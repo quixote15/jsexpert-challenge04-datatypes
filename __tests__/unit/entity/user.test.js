88c95d3a6427d3c0e80e6e44a18bf5d11d083f86
quixote15 <santos2tiago@gmail.com>
2022-02-25 21:08:29 -0300

    feature: finish user entity tests

--- __tests__/unit/entity/user.test.js
+++ __tests__/unit/entity/user.test.js
@@ -1,19 +1,13 @@
-import { 
-    expect,
-    describe,
-    test,
-    jest,
-    beforeEach
-} from '@jest/globals'
+import { expect, describe, test, jest, beforeEach } from "@jest/globals";
+import User from "../../../src/entity/User";
+describe("#UserEntity UnitTesting", () => {
+  it("should instantiate a user entity & display correct string representation", () => {
+    const mockUser = { id: "some-id" };
+    const user = new User(mockUser);
 
-
-describe('#UserEntity UnitTesting', ()=>{
-    it('should instantiate a user entity & display correct string representation', () => {
-        const user = new User({id: 'some-id'})
-
-        expect(user).toBeDefined()
-        expect(user.id).toStrictEqual('some-id')
-        expect(user.toString()).toStrictEqual('')
-
-    })
-})
+    console.log(user);
+    expect(user).toBeDefined();
+    expect(user.id).toStrictEqual(mockUser.id);
+    expect(user.toString()).toStrictEqual(`[id=${mockUser.id}]`);
+  });
+});
